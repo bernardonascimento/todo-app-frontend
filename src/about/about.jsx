7a41eb8728005018e5e35c1f72645d9cbca1b050
bernardonascimento <bernardonasciimento@gmail.com>
2020-04-16 00:16:01 -0300

    New Components: Grid, IconButton, If, PageHeader, TodoForm e Todolist; New functions constructor, handleChange e handleAdd in component Todo; Bind for using this in Todo

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import PageHeader from "../template/pageheader";
 
-export default props => (
+export default (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre " small="Nós " />
+
+    <h2>Nossa História</h2>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s
+    </p>
+
+    <h2>Missão e Valores</h2>
+    <p>
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout.
+    </p>
+
+    <h2>Imprensa</h2>
+    <p>
+      There are many variations of passages of Lorem Ipsum available, but the
+      majority have suffered alteration in some form, by injected humour, or
+      randomised words which don't look even slightly believable
+    </p>
   </div>
 );
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import PageHeader from "../template/pageheader";
+import TodoForm from "./todoform";
+import TodoList from "./todolist";
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      description: "",
+      list: [],
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
-        <h1>Todo</h1>
+        <PageHeader name="Tarefas " small="Cadastro " />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+          handleChange={this.handleChange}
+        />
+        <TodoList />
       </div>
     );
   }
